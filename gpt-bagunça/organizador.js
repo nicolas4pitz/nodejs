cb634e673c7b35732d40ffc879afbd85db0c5072
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-05-23 11:38:51 -0300

    fazendo com fs e globby

--- gpt-bagunça/organizador.js
+++ gpt-bagunça/organizador.js
@@ -1,6 +1,38 @@
-const path = require('path')
 
-console.log(path.resolve("bagunça" ,"foto.jpg"))
 
-console.log(__dirname) //gpt-bagunça
-console.log(__filename) //gpt-bagunça/organizador.js
+import {globby} from 'globby';
+import {rename} from 'fs/promises';
+
+
+async function arrumar(ext, pasta) {
+    const paths = await globby('bagunça',{
+        expandDirectories: {
+            files: ['*'],
+            extensions: [ext]
+        }
+    });
+
+    for(let i = 0; i < paths.length; i++){
+        try{
+            await rename(paths[i], pasta + "/" + paths.values)
+            console.log(`Moved file from ${source} to ${destination}`);
+        } catch (error){
+            console.error(`Got an error trying to move the file: ${error.message}`)
+        }
+        
+    }
+    
+
+    console.log(paths)
+}
+
+
+arrumar('docx', "documento")
+
+
+
+
+// console.log(path.resolve("bagunça" ,"foto.jpg"))
+
+// console.log(__dirname) //gpt-bagunça
+// console.log(__filename) //gpt-bagunça/organizador.js
