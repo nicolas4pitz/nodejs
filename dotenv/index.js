25db746b9eeffab128182df2fd4aa3c759059d88
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-05-26 17:23:13 -0300

    dotenv

---
+++ dotenv/index.js
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+
+const hostname = process.env.HOST
+const db = process.env.DATABASE
+const port = process.env.PORT
+
+console.log(hostname)
+console.log(db)
+console.log(port)
