5caf6b002b4bf997dc5d90ec4a0ef71e59228666
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-05-28 08:30:42 -0300

    dotenv

--- dotenv/index.js
+++ dotenv/index.js
@@ -1,9 +1,13 @@
 import 'dotenv/config'
 
 const hostname = process.env.HOST
-const db = process.env.DATABASE
+const user = process.env.USER
+const api = process.env.API_KEY
+const db = process.env.DB_PASS
 const port = process.env.PORT
 
-console.log(hostname)
-console.log(db)
-console.log(port)
+console.log(`Host do banco de dados ${hostname}`)
+console.log(`Usuario do banco de dados ${user}`)
+console.log(`Senha do banco de dados ${db ? "******" : "NÃO DEFINIDA"}`)
+console.log(`Chave da API: ${api ? "******" : "NÃO DEFINIDA"}`)
+console.log(`Porta do Servidor ${port}`)
