189f6f69cf1d928e09fe612f869b168c21ee8528
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-05-29 08:50:47 -0300

    changes need merge

--- framework-sv/fastify.js
+++ framework-sv/fastify.js
@@ -1,20 +1,25 @@
 import Fastify from "fastify";
 const fastify = Fastify({ logger: true });
 
-const tt = 5000
-
 import 'dotenv/config'
 
-const port = parseInt(process.env.PORT)
+const port = parseInt(process.env.PORT) || 5000
+
+
+
 
 const start = async () => {
     try{
-        await fastify.listen(tt)
+        await fastify.listen({port})
     } catch (error){
-        fastify.log.error("Teste" + error);
+        fastify.log.error("Teste " + error);
         
         process.exit(1)
     }
 }
+
+fastify.get("/items", (req, res) => {
+  res.send({test: "Hello"})
+})
 
 start()
