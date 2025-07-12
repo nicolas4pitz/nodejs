38b8ddbadd1fec750047adcf7f4220dd57411500
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-07-12 18:15:44 -0300

    feat: inicio do onblur

--- vite/sveltekit-app/src/routes/post/new/+page.svelte
+++ vite/sveltekit-app/src/routes/post/new/+page.svelte
@@ -8,17 +8,33 @@
     let corpo = $state('');
 
     let confirEl = document.createElement("span")
-    confirEl.textContent = "Criado";
-    confirEl.setAttribute("class", "bg-lime-500 w-16 p-1 rounded-2xl m-auto text-white transition-shadow mt-4 mb-4")
+    let titleConfirmEl = document.createElement("span")
+    let bodyConfirmEl = document.createElement("span")
 
+    confirEl.textContent = "Criado";
+
+    confirEl.setAttribute("class", "bg-lime-500 w-16 p-1 rounded-2xl m-auto text-white transition-shadow mt-4 mb-4")
+    titleConfirmEl.setAttribute("class", "text-red-500 w-16 italic")
+    let forms = document.getElementById("forms")
 
     function submitForms(){
       console.log(`answered question ${title} (${corpo})`)
       
-      let forms = document.getElementById("forms")
+      
 
 
       forms?.appendChild(confirEl)
+
+      setTimeout(() => {
+        confirEl.remove();
+      }, 3000);
+    }
+
+    function validationTitle() {
+      if(title.length > 5){
+        titleConfirmEl.textContent = "titulo errado"
+        forms?.appendChild(titleConfirmEl)
+      }
     }
 
 </script>
@@ -27,10 +43,10 @@
 <section class="flex justify-center">
   <form class="max-w-6xl justify-center w-full bg-gray-200 grid gap-4 mt-3 pt-3 pb-3" id="forms" onsubmit={submitForms}>
     <label for="title">Title</label>
-    <input bind:value={title} type="text" id="title" class="bg-gray-500 p-1">
+    <input bind:value={title} type="text" id="title" class="bg-gray-500 p-1" onblur={validationTitle}>
     <label for="body">Body</label>
     <textarea bind:value={corpo} id="corpo" class="w-2xs h-60 bg-gray-500 mb-2 text-white rounded-ss-sm p-1"></textarea>
-    <button disabled={(!corpo) || corpo.length < 5 || !title} type="submit" class="bg-violet-500 text-white p-1 hover:bg-zinc-500 hover:cursor-pointer">
+    <button disabled={(!corpo) || corpo.length <= 20 || title.length <= 5 || (!title)} type="submit" class="bg-violet-500 text-white p-1 hover:bg-zinc-500 hover:cursor-pointer">
       Publicar Post
     </button>
   </form>
@@ -44,15 +60,5 @@ input{
   border-radius: 2px;
   margin-bottom: 8px;
 }
-
-/* span{
-  background-color: rgb(133, 211, 45);
-  color: white;
-  width: 60px;
-  padding: 4px;
-  border-radius: 12px;
-  margin: 0 auto;
-  transition: 1s ease-in-out;
-} */
 
 </style>
