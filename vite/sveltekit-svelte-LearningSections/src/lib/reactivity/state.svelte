6a980eab75c68b2911e1004686c292d1e9dc083a
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-08-11 20:49:57 -0300

    feat: state file

---
+++ vite/sveltekit-svelte-LearningSections/src/lib/reactivity/state.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+  //No anterior fazer a ingualdade com o 0 já resolvia, agora no 5 é necessário essa diretiva do "$", que ajuda na hora de entender melhor o codigo
+  let count = $state(0);
+  let increment = () => {
+    count++
+  }
+
+</script>
+
+
+<button onclick={increment} class="bg-zinc-600 rounded-2xl p-3 text-white">
+  Incrementar {count}
+</button>
+
+
+
+<style></style>
---
+++ vite/sveltekit-svelte-LearningSections/src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+
+  import State from "$lib/reactivity/state.svelte";
+
+
+</script>
+
+
+<h1>Aprendizado de Svelte e SvelteKit</h1>
+
+<h2>Section 1: Estado (State)</h2>
+
+<div>
+  <State/>
+</div>
+
+
+
+<style>
+
+  *{
+    color: black;
+  }
+
+  h2{
+    color: black;
+    font: normal small-caps bold 34px/1.5 Arial, sans-serif;
+  }
+
+  div{
+    box-sizing: border-box;
+    background-color: chocolate;
+    margin: 10px;
+    padding: 12px;
+  }
+  
+</style>
